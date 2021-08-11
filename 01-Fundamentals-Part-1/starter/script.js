067934f8fcfac56c7ffd8b48e8ed191e9953bd79
sai santhosh <ssai1383@gmail.com>
2021-08-11 14:31:29 -0500

    new1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,37 @@
-let js = "amazing";
-if (js === "amazing") alert("javascript is Fun");
+// Naming convention : camel case or underscore between words
 
-console.log(40 + 8 + 9 - 2);
-console.log(40 + 9 + 656 + 56);
-console.log(40 + 9 + 656 + 56 + 8);
+/* 
+Values and Variables 
+1. Declare variables called 'country', 'continent' and  'population' and
+assign their values according to your own country (population in millions)
+
+2. Log their values to the console
+*/
+let js = "amazing";
+console.log(js);
+
+let firstName = "Sai Santhosh";
+console.log(firstName);
+
+let country = "India";
+let continent = "Asia";
+let population = 1331000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+/* Data Types
+
+1. Number
+2. String
+3. Boolean
+4. Array
+5. Object
+6. Null
+7. Undefined
+8. Function
+9. Symbol
+10. Any
+11. Void
+*/
