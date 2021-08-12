5708b37d9621b1d4c9ed627e98a08b95e7b4c556
sai santhosh <ssai1383@gmail.com>
2021-08-12 15:59:23 -0500

    new

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -7,7 +7,7 @@ assign their values according to your own country (population in millions)
 
 2. Log their values to the console
 */
-let js = "amazing";
+/* let js = "amazing";
 console.log(js);
 
 let firstName = "Sai Santhosh";
@@ -15,11 +15,11 @@ console.log(firstName);
 
 let country = "India";
 let continent = "Asia";
-let population = 1331000000;
+let population = 1331;
 
 console.log(country);
 console.log(continent);
-console.log(population);
+console.log(population); */
 
 //-------------------------------------------------------------------
 /* Data Types
@@ -34,15 +34,16 @@ console.log(population);
 javaScript is a dynamically typed language. we need no explicit data type declaration.
 */
 
-let isIsland = false;
+/* let isIsland = false;
 let language;
 
 console.log(typeof isIsland);
 console.log(typeof language);
 console.log(typeof country);
 console.log(typeof continent);
-console.log(typeof population);
+console.log(typeof population); */
 
+// ------------------------------------------
 /* let, var and const 
 
 1. let is a block scoped variable. mutable
@@ -50,13 +51,73 @@ console.log(typeof population);
 no empty declaration of const variables.
 */
 
-language = "English";
-const country = "India";
-const continent = "Asia";
-const isIsland = false;
-let population = 1331000000;
+/* language = "English"; */
 
+// --------------------------------------
 /* Basic Operators 
-
+let x = 10;
+x +=10; means x = x+10
 +  for strings meaning concatinate strings 
+assignment operator 
+= += -= *= /= %=
+math operators
++ - * / %
+** for power
+comparison operators
+< <= > >=
 */
+
+/* const halfPop = population / 2;
+console.log(halfPop);
+population++;
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+const description =
+  country +
+  " is in " +
+  continent +
+  " and its " +
+  population +
+  " milliion people speak " +
+  language;
+console.log(description); */
+
+/* Operarator Precedence
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence#table
+ */
+
+/* Coding Challenge #1
+Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall.
+GOOD LUCK  */
+
+const markHeight = 1.69;
+const markMass = 78;
+const johnHeight = 1.95;
+const johnMass = 92;
+
+const markBMI = markMass / markHeight ** 2;
+const johnBMI = johnMass / johnHeight ** 2;
+
+const markHigherBMI = markBMI > johnBMI;
+
+console.log(markBMI, johnBMI, markHigherBMI);
+
+/* Strings and Template Literlas 
+
+const name= `I'm ${firstName}`
+Type conversion */
