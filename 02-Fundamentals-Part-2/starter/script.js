81d1822ac157056600f2259488d1165d97cb2b2a
sai santhosh <ssai1383@gmail.com>
2021-08-15 09:00:33 -0500

    new

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -238,3 +238,89 @@ const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 console.log(total); */
 
 /* Intro to Objects */
+/* const myCountry = {
+  country: "USA",
+  capital: "WDC",
+  language: "English",
+  population: 333,
+  neighbours: ["Canada", "Mexico"],
+};
+
+console.log(
+  `${myCountry.country} has ${myCountry.population} Million people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}`
+);
+
+myCountry.location = "NA";
+console.log(myCountry);
+
+myCountry["states"] = 50; */
+
+/* Object Methods
+ calcAge: function () {
+    this.age = 2037 - this.birthYeah;
+    return this.age;
+  },
+
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()}-year old ${jonas.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`
+  }
+};
+
+console.log(jonas.calcAge());
+
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.age);
+*/
+/* Coding Challenge #3
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to
+implement the calculations! Remember: BMI = mass / height ** 2 = mass
+/ (height * height) (mass in kg and height in meter)
+Your tasks:
+1. For each of them, create an object with properties for their full name, mass, and
+height (Mark Miller and John Smith)
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same
+method on both objects). Store the BMI value to a property, and also return it
+from the method
+3. Log to the console who has the higher BMI, together with the full name and the
+respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m
+tall. */
+
+/* const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.BMI = this.mass / (this.height * this.height);
+    return this.BMI;
+  },
+};
+
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.BMI = this.mass / (this.height * this.height);
+    return this.BMI;
+  },
+};
+
+const higherBMI = function () {
+  if (mark.calcBMI() < john.calcBMI()) {
+    return `${john.fullName}'s BMI(${john.BMI}) is higher than ${mark.fullName}'s${mark.BMI}`;
+  } else {
+    return `${mark.fullName}'s BMI(${mark.BMI}) is higher than ${john.fullName}'s(${john.BMI})`;
+  }
+};
+
+console.log(higherBMI()); */
+
+/* iteration: the for Loop */
+
+/* for (let voter = 1; voter <= 50; voter++) {
+  console.log(`Voter number ${voter} is currently voting`);
+} */
+
+/* Looping Arrays, Breaking and Continuing */
